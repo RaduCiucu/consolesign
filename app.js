1cc98ef7f74367a778f96523c1d5e1db9dbebc43
Radu Ciucu <Radu.Ciucu@gmail.com>
2020-04-28 15:58:05 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var bt = "font-weight:bold;";
+var headc = "text-transform: uppercase; font-size:20px; font-weight: 900; letter-spacing:0; line-height:1; font-family: 'Poppins'; color:#013d7a ;";
+var messc = "font-size:16px; font-weight:200; letter-spacing:0.1em; line-height:1.4em; font-family:'Poppins'; color:rgba(0,0,25,0.5);";
+var rad = "\n%c   ppppp                                  pppppp  \n%c )bbPPPbbbbp                           pbbbPPPbbb \n%c bbP     )bbbp    pppbbbbbbbbbppp   pbbPC      bbP\n%c bbP )bbbbp )bbbbbbbbbbbbbbbbbbbbbbbbP  pbbbb  bbP\n%c Qbb )bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbD )bb \n%c )bb  QbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbC pbP \n%c  )bb pbbbbbbbPPPP PPbbbbbbbbPP  PPPbbbbbbbbp)bb  \n%c   QbbbbbbPP          QbbbbP          )bbbbbbbbC  \n%c   pbbbbP   pQbbbbbpc )bbbbP  pbbbbbbpp  )bbbbbp  \n%c  (bbbP  pbbbb     )bb)bbbbC)bb(    )bbbp  )bbbb  \n%c  bbbC )bbbbbb     )bbQbbbbbQbb      bbbbbp  Qbbb \n%c (bbP )bbbbbbbbppppbP bbbbbb Sbbppppbbbbbbbb  bbb \n%c Qbb  bbbbbbbbbbbP    QbbbbP    PbbbbbbbbbbbC )bbP\n%c )bb )bbbbbbbbbP      bbbbbbC     )bbbbbbbbbP )bb \n%c  bbb bbbbbbbbb    qp )bbbb( qp    bbbbbbbbb  bbP \n%c   bbb bbbbbbbb    )Qpp    qqQ(    bbbbbbbbC)bbP  \n%c    bbbp)bbbbbbb                  pbbbbbbP pbbC   \n%c      Sbbbp)PbbbP                 bbbPC pbbbC     \n%c        )Pbbbppp                   ppDbbPP        \n%c            )PPbbbbbbbppppppDbbbbbbbPP            \n";
+
+console.log(rad, bt, bt, bt, bt, bt, bt, bt, bt, bt, bt, bt, bt, bt,bt, bt, bt, bt, bt, bt , bt);
+console.log("%c ⚡ Hello There! ⚡", headc);
+console.log("%c What are you doing Down here❓", messc);
+console.log("%c You are Missing all the Fun Stuff 🦝", headc);
+console.log("%c If you want to check my code and my projects go to  http://github.com/raduciucu 💻", messc);
